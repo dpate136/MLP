04734d3860f0f45151d07d80ef9bb3cb5b7820c1
dpate136 <51708015+dpate136@users.noreply.github.com>
2020-05-11 11:14:18 -0400

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -9,7 +9,10 @@ Created on Thu Feb 14 13:13:09 2019
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 import matplotlib.pyplot as plt
+
 
 #import the dataset
 dataset = pd.read_csv('filename.csv')
@@ -43,13 +46,10 @@ y_pred = mlp.predict(X_test).reshape(-1,1)
 inv_pred = scaler.inverse_transform(y_pred)
 inv_test = scaler.inverse_transform(y_test)
 
-from sklearn.metrics import mean_squared_error
-from math import sqrt
+#RMSE
 rms1 = sqrt(mean_squared_error(inv_pred, inv_test))
 
 #mape
-def mean_absolute_percentage_error(inv_pred, inv_test): 
-    inv_pred, inv_test = np.array(inv_test), np.array(inv_pred)
 mape1 = np.mean(np.abs((inv_pred, inv_test) / inv_test)) * 100
 
 plt.plot(inv_test, inv_pred, lable = "actual vs forecast")
